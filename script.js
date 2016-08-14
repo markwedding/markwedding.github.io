5e2999f6659c382ed989561ca53100a89fa3113c
markwedding <markwedding10@gmail.com>
2016-08-14 11:55:01 -0400

    Added projects and completed version 1 of the site

---
+++ script.js
@@ -0,0 +1,34 @@
+var skills = [
+  ['HTML',4],
+  ['CSS',4],
+  ['Bootstrap',4],
+  ['PHP',4],
+  ['SQL',4],
+  ['VBA',4],
+  ['JavaScript',3],
+  ['jQuery',3],
+  ['MySQL',3],
+  ['Semantic UI',2],
+  ['Python',2],
+  ['SQL Server',2],
+  ['VB.NET',2],
+  ['WordPress',2],
+  ['AngularJS',1]
+];
+
+var html = "";
+
+for(var i = 0; i < skills.length; i++) {
+  html += '<tr>';
+  html += '<td class="right aligned eight wide">';
+  html += '<h4 class="ui inverted header">' + skills[i][0] + '</h4>';
+  html += '</td>';
+  html += '<td class="eight wide">';
+  for (var j = 0; j < skills[i][1]; j++) {
+    html += '<div class="ui teal empty circular label"></div>';
+  }
+  html += '</td>';
+  html += '</tr>';
+}
+
+document.getElementById("skills-body").innerHTML = html;
---
+++ test.js
@@ -0,0 +1,3 @@
+document.write("Document write method");
+document.getElementById("heading1").innerHTML = "Inner HTML Method";
+document.getElementById("demo").innerHTML = 5 + 6;
