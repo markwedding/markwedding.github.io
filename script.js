240230dc013be81159b5805658f7a4872688790c
markwedding <markwedding10@gmail.com>
2016-08-25 17:52:42 -0400

    Changed the general skills section so that it is now generated by JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+function writeGenSkill(icon, heading, paragraph) {
+  var skill = "";
+
+  skill += '<h2 class="ui inverted header">';
+  skill += '<i class="' + icon + ' inverted teal icon"></i>';
+  skill += '<div class="content">' + heading + '</div></h2>';
+  skill += '<p class="p-m">' + paragraph + '</p>';
+  return skill;
+}
+
+var generalSkills = [
+  {
+    icon:'html5',
+    heading:'Web Development',
+    paragraph:'I know how to create custom websites combining HTML, CSS, JavaScript, ' +
+      'front-end frameworks like Bootstrap, and a PHP/MySQL back end.'
+  },
+  {
+    icon:'database',
+    heading:'Database Development',
+    paragraph:'I have a strong understanding of relational databases and know ' +
+      'how to design an effective database structure for the back end of an application.'
+  },
+  {
+    icon:'line chart',
+    heading:'Data Analytics',
+    paragraph:'I can understand and make predictions from large data sets. I have ' +
+      'experience with mathematical modeling techniques such as simulation and linear programming.'
+  },
+  {
+    icon:'wordpress',
+    heading:'Website Management',
+    paragraph:'I have used WordPress to update content on a website and have ' +
+      'implemented various plug-ins related to content presentation, user forms, and payment processing.'
+  }
+];
+
+var genSkills_html = "";
+
+for(var j = 0; j < generalSkills.length; j++) {
+  genSkills_html += writeGenSkill(generalSkills[j].icon, generalSkills[j].heading, generalSkills[j].paragraph);
+  document.getElementById('gen-skills').innerHTML = genSkills_html;
+}
+
+$('#gen-skills p:last-child').removeClass("p-m");
+
+
+
 var skills = [
   ['HTML',4],
   ['CSS',4],
